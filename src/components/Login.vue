60ba0a7e82309756805624d168ff2f9d92b6ebdc
gaohuan214 <18855972539@163.com>
2018-12-21 20:18:52 +0800

    表单重置功能完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="login">
-  <el-form ref="form" :model="form" label-width="80px">
-      <img src="@/assets/avatar.jpg" alt="">
-      <el-form-item label="用户名">
-        <el-input v-model="form.name" placeholder="请输入用户名"></el-input>
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px" status-icon>
+      <img src="@/assets/avatar.jpg" alt>
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" placeholder="密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">登录</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
-  </el-form>
-</div>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -21,17 +21,31 @@ export default {
   data() {
     return {
       form: {
-        name: "",
-        password: ""
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'change' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'change' },
+          { min: 5, max: 9, message: '长度在 5 到 9 个字符', trigger: 'change' }
+        ]
       }
-    };
+    }
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    login() {
+      console.log('submit!')
+    },
+    reset() {
+      // resetFields 对整个表单进行重置,包括校验结果和图标
+      this.$refs.form.resetFields()
     }
   }
-};
+}
 </script>
 
 <style lang = "less">
